c859818a6eb41b000699d6fa73caf910f12767b1
2rjswn <h47841930@gmail.com>
2025-03-06 14:35:41 +0900

    start

---
+++ src/main/java/com/washer/Things/ThingsApplication.java
@@ -0,0 +1,13 @@
+package com.washer.Things;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThingsApplication.class, args);
+	}
+
+}
