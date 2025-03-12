790a32610d1128a93143fa17d826cf3152306986
2rjswn <h47841930@gmail.com>
2025-03-12 09:42:06 +0900

    edit auth

--- src/main/java/com/washer/Things/domain/auth/presentation/AuthController.java
+++ src/main/java/com/washer/Things/domain/auth/presentation/AuthController.java
@@ -13,8 +13,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@RequestMapping("/auth")
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
