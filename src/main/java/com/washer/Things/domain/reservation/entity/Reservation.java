ed58773a8cb7a2f20609562e2cd4ec1f79296eb7
2rjswn <h47841930@gmail.com>
2025-05-28 21:14:24 +0900

    add smartthongs oauth

--- src/main/java/com/washer/Things/domain/reservation/entity/Reservation.java
+++ src/main/java/com/washer/Things/domain/reservation/entity/Reservation.java
@@ -26,6 +26,7 @@ public class Reservation {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "machine_id", nullable = false)
     private Machine machine;
