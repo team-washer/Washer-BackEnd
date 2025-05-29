d53d6560b9caa4e9f1b1172a6e06ed3671912c22
2rjswn <h47841930@gmail.com>
2025-05-29 14:55:21 +0900

    add healthy

--- src/main/java/com/washer/Things/domain/smartThingsToken/presentation/SmartThingsTokenController.java
+++ src/main/java/com/washer/Things/domain/smartThingsToken/presentation/SmartThingsTokenController.java
@@ -3,13 +3,12 @@ package com.washer.Things.domain.smartThingsToken.presentation;
 import com.washer.Things.domain.smartThingsToken.service.SmartThingsTokenService;
 import com.washer.Things.global.exception.dto.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.washer.Things.domain.smartThingsToken.presentation.dto.response.SmartThingsTokenResponse;
 @RestController
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/oauth")
 public class SmartThingsTokenController {
@@ -25,5 +24,16 @@ public class SmartThingsTokenController {
             SmartThingsTokenResponse tokenResponse = smartThingsTokenService.exchangeCode(code);
         smartThingsTokenService.saveToken(tokenResponse);
         return ResponseEntity.ok(ApiResponse.success("SmartThings 인증 및 토큰 저장 성공"));
+    }
+
+    @PostMapping("/smartapp")
+    public ResponseEntity<String> handleSmartAppWebhook(@RequestBody String payload) {
+        log.info("SmartApp Webhook payload received: {}", payload);
+
+        if (payload.contains("\"lifecycle\":\"PING\"")) {
+            return ResponseEntity.ok("{\"pingData\": {\"status\": \"OK\"}}");
+        }
+
+        return ResponseEntity.ok("OK");
     }
 }
