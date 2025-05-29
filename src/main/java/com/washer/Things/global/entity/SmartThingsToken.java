21704a772eab918ce1c8b3203ec7c665f6e8b7bc
2rjswn <h47841930@gmail.com>
2025-05-29 09:15:48 +0900

    add token entity

---
+++ src/main/java/com/washer/Things/global/entity/SmartThingsToken.java
@@ -0,0 +1,31 @@
+package com.washer.Things.global.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "smartthings_tokens")
+public class SmartThingsToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userId; // 사용자 식별용(로그인 유저 아이디)
+
+    private String accessToken;
+
+    private String refreshToken;
+
+    private Instant issuedAt;
+
+    private Instant expiresAt;
+}
