cfc7adcfd0095b16debcc4164e317cfcd0fb2f63
2rjswn <h47841930@gmail.com>
2025-06-02 10:29:25 +0900

    update

--- src/main/java/com/washer/Things/domain/smartThingsToken/presentation/SmartThingsTokenController.java
+++ src/main/java/com/washer/Things/domain/smartThingsToken/presentation/SmartThingsTokenController.java
@@ -18,10 +18,10 @@ public class SmartThingsTokenController {
         return ResponseEntity.ok(ApiResponse.success(devices, "SmartThings 디바이스 조회 성공"));
     }
 
-    @GetMapping("/callback")
-    public ResponseEntity<ApiResponse<Void>> callback(@RequestParam("code") String code) {
-            SmartThingsTokenResponse tokenResponse = smartThingsTokenService.exchangeCode(code);
-        smartThingsTokenService.saveToken(tokenResponse);
-        return ResponseEntity.ok(ApiResponse.success("SmartThings 인증 및 토큰 저장 성공"));
-    }
+//    @GetMapping("/callback")
+//    public ResponseEntity<ApiResponse<Void>> callback(@RequestParam("code") String code) {
+//            SmartThingsTokenResponse tokenResponse = smartThingsTokenService.exchangeCode(code);
+//        smartThingsTokenService.saveToken(tokenResponse);
+//        return ResponseEntity.ok(ApiResponse.success("SmartThings 인증 및 토큰 저장 성공"));
+//    }
 }
